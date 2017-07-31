8e9d83f8bf1c978d8f2a914cfa039c443bca5f0a
leejung <ronlystar@hanmail.net>
2017-07-31 12:04:59 +0900

    Update

---
+++ 기타/11718_그대로출력하기.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+using namespace std;
+
+int main()
+{
+	freopen("input.txt", "r", stdin);
+	
+	char input;
+	while (~scanf("%c", &input)) {
+		printf("%c", input);
+	}
+
+}
